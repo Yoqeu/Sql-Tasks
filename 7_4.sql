a131b796d538e7be34c7103724c795ec26c168a0
Yoqeu <47595796+Yoqeu@users.noreply.github.com>
2020-12-12 00:25:11 +0300

    Update 7_4.sql

--- 7_4.sql
+++ 7_4.sql
@@ -1,3 +1,4 @@
+use students;
 select 'name' as name,  min(char_length(name)) as min,  max(char_length(name)) as max,  avg(char_length(name)) as average 
 from name union all select 'surname' as table_name,  min(char_length(surname)) as min,  max(char_length(surname)) as max,  avg(char_length(surname)) as average from surname union all 
 select 'country' as table_name,  min(char_length(country_name)) as min,  max(char_length(country_name)) as max,  avg(char_length(country_name)) as average from country union all 
