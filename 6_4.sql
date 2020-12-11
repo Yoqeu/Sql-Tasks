7235d23a638bc25b9f4ba7d627a6f2af7b734132
Yoqeu <47595796+Yoqeu@users.noreply.github.com>
2020-12-12 00:24:39 +0300

    Update 6_4.sql

--- 6_4.sql
+++ 6_4.sql
@@ -1,2 +1,3 @@
+use students;
 select count(case when name like concat('%', lower(substring('Глеб', 1, 2)), 
 '%') then name else null end) as two, count(case when name like concat('%', lower(substring('Глеб', 1, 3)), '%') then name else null end) as one from name;
