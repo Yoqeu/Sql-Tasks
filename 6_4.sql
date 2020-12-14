949f7b0fb17e38dfc8f55b17f73e412df69298f8
Yoqeu <47595796+Yoqeu@users.noreply.github.com>
2020-12-14 18:03:03 +0300

    Update 6_4.sql

--- 6_4.sql
+++ 6_4.sql
@@ -1,2 +1,2 @@
-use students;
+use MyData
 select count(case when name like concat('%', replace('Глеб', right('Глеб', 2),''), '%') then name else null end) as two, count(case when name like concat('%', replace('Глеб', right('Глеб', 1),''), '%') then name else null end) as one from name; 
