e3bc74ee25bff98e4589f79b8cbf3122b668a36c
Yoqeu <47595796+Yoqeu@users.noreply.github.com>
2020-12-15 02:33:55 +0300

    Update 1_5.sql

--- 1_5.sql
+++ 1_5.sql
@@ -7,6 +7,6 @@ insert into stud(id_name, id_sname, id_patr, id_pers) select (rand() * (name_max
 (rand() * 999999 + 1) from name, surname, middle_name_buffer,  
 (select max(id) as name_max from name) as f_name,  
 (select max(id) as surname_max from surname) as s_name,  
-(select max(id) as middle_name_max from middle_name_buffer) as m_name limit 1000000; 
+(select max(id) patr_max from middle_name_buffer) as m_name limit 1000000; 
  
 set foreign_key_checks = 1;
