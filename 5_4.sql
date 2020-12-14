8eae1f8c37f5441a557e7be1bab73382af31b475
Yoqeu <47595796+Yoqeu@users.noreply.github.com>
2020-12-14 18:02:46 +0300

    Update 5_4.sql

--- 5_4.sql
+++ 5_4.sql
@@ -1,3 +1,3 @@
-use students;
+use MyData;
 select count(case when surname like concat('%', replace('Ряжченко', right('Ряжченко', 2),''), '%') then surname else null end) as two, count(case when surname like concat('%', replace('Ряжченко', 
 right('Ряжченко', 1),''), '%') then surname else null end) as one from surname; 
