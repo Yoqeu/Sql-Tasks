6cf4200726a6432c0d88ed6dc88b7003bd3baf49
Yoqeu <47595796+Yoqeu@users.noreply.github.com>
2020-12-15 04:35:02 +0300

    Update 9_5.sql

--- 9_5.sql
+++ 9_5.sql
@@ -4,13 +4,13 @@ truncate table pers;
  
 insert into pers(id_gender, id_citizenship, birth_date, letter_pass, pass, id_addr1, id_addr2, id_addr3,  enrollment, id_univ, id_fac, id_spec, id_dep, id_course, id_form, id_budget, timestamp) select  
 (rand() * (gender_max - 1) + 1), 
-(rand() * (citizenship_max - 1) + 1), 
+(rand() * (cntr_max - 1) + 1), 
 from_unixtime(unix_timestamp('2000-1-1') + floor(rand()* 
 (unix_timestamp('2025-1-1')-unix_timestamp('2000-1-1')+1)),"%Y-%m-%d"),
 substring(md5(rand()), 1, 2), rand() * 999998 + 1,
-(rand() * (address_max - 1) + 1), 
-(rand() * (address_max - 1) + 1), 
-(rand() * (address_max - 1) + 1),  
+(rand() * (addr_max - 1) + 1), 
+(rand() * (addr_max - 1) + 1), 
+(rand() * (addr_max - 1) + 1),  
 from_unixtime(unix_timestamp('2000-1-1') + floor(rand()* 
 (unix_timestamp('2025-1-1')-unix_timestamp('2000-1-1')+1)),"%Y-%m-%d"),
 (rand() * (univ_max - 1) + 1), 
@@ -21,7 +21,8 @@ from_unixtime(unix_timestamp('2000-1-1') + floor(rand()*
 (rand() * (form_max - 1) + 1), 
 (rand() * (budget_max - 1) + 1),
 from_unixtime(unix_timestamp('2000-1-1') + floor(rand()* 
-(unix_timestamp('2025-1-1')-unix_timestamp('2000-1-1')+1)),"%Y-%m-%d"),  
+(unix_timestamp('2025-1-1')-unix_timestamp('2000-1-1')+1)),"%Y-%m-%d")
+
 from gender, country, addr, univer, fac, spec, dep, course, form, course, budgetform, 
 (select max(id) as gender_max from gender) as gender_t,
 (select max(id_pk) as cntr_max from country) as cntr_t,  
@@ -32,6 +33,6 @@ from gender, country, addr, univer, fac, spec, dep, course, form, course, budget
 (select max(id) as dep_max from dep) as dep_t,  
 (select max(id) as course_max from course) as course_t,  
 (select max(id) as form_max from form) as form_t,  
-(select max(id) as budgetform_max from budgetform) as budgetform_t  limit 1000000; 
+(select max(id) as budget_max from budgetform) as budgetform_t  limit 1000000; 
  
 set foreign_key_checks = 1;
