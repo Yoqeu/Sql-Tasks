9679e1279166932e032a8e774d1e9d7b874c0882
Yoqeu <47595796+Yoqeu@users.noreply.github.com>
2020-12-15 04:32:34 +0300

    Update 4_5.sql

--- 4_5.sql
+++ 4_5.sql
@@ -16,7 +16,7 @@ insert into tchr(id_name, id_sname, id_patr, id_pers) select (rand() * (name_max
 (rand() * (patr_max - 1) + 1),  
 (rand() * 999999 + 1) from name, surname, middle_name_buffer, 
 (select max(id) as name_max from name) as f_name,  
-(select max(id) as surname_max from surname) as s_name,  
+(select max(id) as sname_max from surname) as s_name,  
 (select max(id) as patr_max from middle_name_buffer) as m_name limit 10000; 
  
 insert into result(id_stud, id_subj, id_mark, id_tchr, date) select (rand() * (stud_max - 1) + 1), 
@@ -24,5 +24,5 @@ insert into result(id_stud, id_subj, id_mark, id_tchr, date) select (rand() * (s
 (unix_timestamp('2025-1-1')-unix_timestamp('2000-1-1'))+1),"%Y-%m-%d") from stud, subj, mark, tchr, 
 (select max(id) as stud_max from stud) as stud_t, 
 (select max(id) as subj_max from subj) as subj_t, 
-(select max(id) as mark_max from mark) as mark_t 
+(select max(id) as mark_max from mark) as mark_t,
 (select max(id) as tchr_max from tchr) as tchr_t limit 1000000; 
