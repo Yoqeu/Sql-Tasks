4122f4081db35438315c143cb778be167962caa4
Yoqeu <47595796+Yoqeu@users.noreply.github.com>
2021-05-08 21:59:44 +0300

    Update 5_fake_insert.sql

--- 5_fake_insert.sql
+++ 5_fake_insert.sql
@@ -1,3 +1,3 @@
 SELECT 1 as id, "yoqe" as login, "c6b62bc3d50da64af16ecd18aee4fb31282162d4" as password_hash, "2021-08-07 21:24:50" as access_time, "authorized" as access_result
 UNION
-SELECT 2, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized";
+SELECT 2, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-07 21:25:30", "authorized";
