6d3bb34f4a7739d80c9e0228704071fc302b928a
Yoqeu <47595796+Yoqeu@users.noreply.github.com>
2021-05-08 21:58:35 +0300

    Update 5_fake_update.sql

--- 5_fake_update.sql
+++ 5_fake_update.sql
@@ -1,13 +1,13 @@
-SELECT 1 as id, "yoqe" as login, "c6b62bc3d50da64af16ecd18aee4fb31282162d4" as password_hash, "2021-08-04 20:34:50" as access_time, "authorized" as access_result
+SELECT 1 as id, "yoqe" as login, "c6b62bc3d50da64af16ecd18aee4fb31282162d4" as password_hash, "2021-08-07 21:55:00" as access_time, "authorized" as access_result
 UNION
-SELECT 2, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
+SELECT 2, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-07 21:55:53", "authorized"
 UNION
-SELECT 3, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
+SELECT 3, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-07 21:55:53", "authorized"
 UNION
-SELECT 4, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
+SELECT 4, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-07 21:55:53", "authorized"
 UNION
-SELECT 5, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
+SELECT 5, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-07 21:55:53", "authorized"
 UNION
-SELECT 6, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
+SELECT 6, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-07 21:55:53", "authorized"
 UNION
-SELECT 7, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized";
+SELECT 7, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-07 21:55:53", "authorized";
