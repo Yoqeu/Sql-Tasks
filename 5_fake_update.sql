702e9688ea4bc8905fba3f12ec284d4c9cd0418d
Yoqeu <47595796+Yoqeu@users.noreply.github.com>
2021-05-08 21:18:29 +0300

    Add files via upload

--- 5_fake_update.sql
+++ 5_fake_update.sql
@@ -1,13 +1,13 @@
-SELECT 1 as id, "explosion204" as login, "c6b62bc3d50da64af16ecd18aee4fb31282162d4" as password_hash, "2021-08-04 20:34:50" as access_time, "authorized" as access_result
+SELECT 1 as id, "yoqe" as login, "c6b62bc3d50da64af16ecd18aee4fb31282162d4" as password_hash, "2021-08-04 20:34:50" as access_time, "authorized" as access_result
 UNION
-SELECT 2, "explosion204", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
+SELECT 2, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
 UNION
-SELECT 3, "explosion204", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
+SELECT 3, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
 UNION
-SELECT 4, "explosion204", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
+SELECT 4, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
 UNION
-SELECT 5, "explosion204", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
+SELECT 5, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
 UNION
-SELECT 6, "explosion204", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
+SELECT 6, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized"
 UNION
-SELECT 7, "explosion204", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized";
+SELECT 7, "yoqe", "fb8af6a94ae539f7eed0dd3a54a43afe10dd81b4", "2021-08-04 20:36:22", "authorized";
